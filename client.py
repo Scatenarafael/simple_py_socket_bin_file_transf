c1f1af5c365948165da25e7440518ad60e5d22c7
rafael.scatena <rafael.scatena@itech.org.br>
2024-01-11 12:54:45 -0300

    working...

--- client.py
+++ client.py
@@ -1,47 +1,53 @@
 import socket
 import os
-
+import time
 
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 4456
 ADDR = (IP, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
+HEADER_SIZE = 128
 
 
 def handle_send_file(
         files_folder, file_name, client):
-
     file_name_path = os.path.join(files_folder, file_name)
 
     file_size = os.path.getsize(file_name_path)
-    
-    client.send(f"{file_name}@{str(file_size)}".encode(FORMAT))
+
+    b_filename = bytes(file_name, FORMAT)
+
+    b_filesize = bytes(str(file_size), FORMAT)
+
+    # client.send(f"{file_name}@{str(file_size)}".encode(FORMAT))
+
+    filename_length = len(b_filename)
+    print("filename_length >> ", filename_length)
+
+    filesize_length = len(b_filesize)
+    print("filesize_length >> ", filesize_length)
+
+    data = b_filename + b'@' + b_filesize + b' ' * \
+        (HEADER_SIZE - (filename_length + filesize_length + len(b'@')))
 
     with open(file_name_path, 'rb') as f:
-        data = f.read(file_size)
+        data += f.read(file_size)
     client.send(data)
+    time.sleep(8)
 
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
     files = os.listdir("files")
-
+    print('files >> ', files)
     # for file in os.listdir("files"):
     #     handle_send_file("files", file, client)
-    i = 0
+    handle_send_file("files", files[0], client)
+    handle_send_file("files", files[1], client)
+    handle_send_file("files", files[2], client)
 
-    handle_send_file("files", files[i], client)
-
-    while True:
-        msg = client.recv(SIZE).decode(FORMAT)
-        print("while >> msg:", msg)
-        if msg == "next" and i+1 < len(files):
-            i += 1
-            handle_send_file("files", files[i], client)
-        else:
-            break
     print("Disconnected from the server.")
     client.close()
 
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socket
 import threading
 import os
 import tqdm
+import time
 
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 4456
@@ -10,51 +11,67 @@ ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 SERVER_DATA_PATH = "server_data"
+HEADER_SIZE = 128
+
+
+def receive_file(conn, addr):
+    while True:
+        msg = conn.recv(HEADER_SIZE).decode(FORMAT)
+        if msg:
+            file_name, file_size = msg.split("@")
+            print("file_name: ", file_name)
+            print("file_size: ", file_size)
+
+            progress = tqdm.tqdm(
+                # range(int(file_size)),
+                f"Receiving {file_name}",
+                unit="B",
+                unit_scale=True,
+                unit_divisor=1024,
+                total=int(file_size)
+            )
+
+            data = b''
+            t1 = time.perf_counter()
+
+            while len(data) < int(file_size):
+                received = conn.recv(512)
+                data += received
+                progress.update(512)
+
+            t2 = time.perf_counter()
+
+            print("time receive file >>> ", (t2 - t1))
+            print("len(data) >>> ", len(data))
+            try:
+                t3 = time.perf_counter()
+                with open(f"uploads/received_{file_name}", 'wb') as f:
+                    f.write(data)
+
+                if (os.path.exists(f"uploads/received_{file_name}") and
+                        os.path.getsize(f"uploads/received_{file_name}") > 0):
+                    t4 = time.perf_counter()
+
+                    print("time to write file >>> ", (t4 - t3))
+                    print("File Successfuly Saved!")
+                else:
+                    print("Could no save file")
+            except FileNotFoundError:
+                print("Something went wrong")
+                conn.close()
+            # print(f"[DISCONNECTED] {addr} disconnected")
+            # conn.close()
+        else:
+            break
 
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
-    msg = conn.recv(SIZE).decode(FORMAT)
 
-    file_name, file_size = msg.split("@")
-    print("file_name: ", file_name)
-    print("file_size: ", file_size)
+    receive_file(conn, addr)
 
-    progress = tqdm.tqdm(
-                    range(int(file_size)),
-                    "Receiving img.jpg",
-                    unit="B",
-                    unit_scale=True,
-                    unit_divisor=1024
-                    )
-
-    data = b''
-    while True:
-        received = conn.recv(1024)
-        if not received:
-            break
-        else:
-            data += received
-        progress.update(1024)
-
-    print("len(data) >>> ", len(data))
-    try:
-        with open(f"uploads/received_{file_name}", 'wb') as f:
-            f.write(data)
-
-        if (os.path.exists(f"uploads/received_{file_name}") and
-                os.path.getsize(f"uploads/received_{file_name}") > 0):
-            conn.send("next".encode(FORMAT))
-            print("File Successfuly Saved!")
-        else:
-            conn.send("finished".encode(FORMAT))
-            print("Could no save file")
-    except FileNotFoundError:
-        print("Something went wrong")
-        conn.close()
-
-    print(f"[DISCONNECTED] {addr} disconnected")
-    conn.close()
+    # print(f"[DISCONNECTED] {addr} disconnected")
+    # conn.close()
 
 
 def main():
@@ -68,6 +85,7 @@ def main():
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+        thread.join()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 
